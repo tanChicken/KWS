e615beaf99bc988fe1f6fe47be2a6bc26a00e562
BepisChoo <chooasaa@gmail.com>
2026-07-07 22:18:15 +0800

    changed images for about page and k-wave tour, changed hero image for grand classic tour

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -73,7 +73,7 @@ export default function AboutPage() {
                 /> */}
                 <div className="relative w-full h-[420px] md:h-[600px] rounded overflow-hidden shadow-soft-lg">
                   <Image
-                    src="/assets/stitch/about-new.jpg"
+                    src="/assets/stitch/about-park.jpg"
                     alt={t("storyImageAlt")}
                     fill
                     sizes="(max-width: 1024px) 100vw, 50vw"
